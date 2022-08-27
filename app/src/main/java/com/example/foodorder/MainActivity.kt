60ba44ceae626316661855408c303c04bc2152ff
Osuolale <mohd5289@yahoo.com>
2022-08-27 21:51:49 +0100

    initial commit

---
+++ app/src/main/java/com/example/foodorder/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.foodorder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.foodorder.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        binding=    DataBindingUtil.setContentView(this, R.layout.activity_main)
+  val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.apply {
+           btmNav.setupWithNavController(navController)
+        }
+
+    }
+}
